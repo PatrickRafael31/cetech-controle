1ce78cf9383bdfb9a6d10a2f5f5b4ca3ea045fce
PatrickRafael31 <cetechinformaticapr@gmail.com>
2026-05-16 17:36:25 -0300

    Projeto inicial Cetech

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔌 CONEXÃO COM MYSQL
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "28042015", // coloque sua senha se tiver
+  database: "cetech",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Erro ao conectar:", err);
+  } else {
+    console.log("🔥 MySQL conectado!");
+  }
+});
+
+// 📌 LISTAR CLIENTES
+app.get("/clientes", (req, res) => {
+  db.query("SELECT * FROM clientes", (err, result) => {
+    if (err) return res.status(500).json(err);
+    res.json(result);
+  });
+});
+
+// 📌 CADASTRAR CLIENTE
+app.post("/clientes", (req, res) => {
+  console.log("CHEGOU NO BACKEND");
+
+  const { nome, sistema, custo, venda, dia_gdoor, dia_cetech } = req.body;
+
+  const sql = `
+    INSERT INTO clientes 
+    (nome, sistema, custo, venda, dia_vencimento_gdoor, dia_vencimento_cetech) 
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql, [nome, sistema, custo, venda, dia_gdoor, dia_cetech], (err) => {
+    if (err) {
+      console.log("ERRO:", err);
+      return res.status(500).json(err);
+    }
+
+    console.log("SALVO COM SUCESSO");
+    res.json({ ok: true });
+  });
+});
+
+// 📌 EXCLUIR
+app.delete("/clientes/:id", (req, res) => {
+  db.query("DELETE FROM clientes WHERE id = ?", [req.params.id], (err) => {
+    if (err) return res.status(500).json(err);
+    res.json({ ok: true });
+  });
+});
+
+// 📌 EDITAR
+app.put("/clientes/:id", (req, res) => {
+  const { nome, sistema, custo, venda } = req.body;
+
+  db.query(
+    "UPDATE clientes SET nome=?, sistema=?, custo=?, venda=? WHERE id=?",
+    [nome, sistema, custo, venda, req.params.id],
+    (err) => {
+      if (err) return res.status(500).json(err);
+      res.json({ ok: true });
+    },
+  );
+});
+
+// 🚀 INICIAR
+app.listen(3000, () => {
+  console.log("🚀 Servidor rodando em http://localhost:3000");
+});
